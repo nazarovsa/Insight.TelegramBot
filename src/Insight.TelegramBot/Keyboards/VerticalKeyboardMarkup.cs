63988027a87ea7307042b61974a4db66199c4bd7
Sergey Nazarov <insight.appdev@gmail.com>
2020-09-06 14:19:17 +0300

    VerticalKeyboardMarkup now implements IEnumerable<IEnumerable<IKeyboardButton>>

--- src/Insight.TelegramBot/Keyboards/VerticalKeyboardMarkup.cs
+++ src/Insight.TelegramBot/Keyboards/VerticalKeyboardMarkup.cs
@@ -1,59 +1,66 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Insight.TelegramBot.Keyboards
 {
-    public sealed class VerticalKeyboardMarkup
-    {
-        private readonly List<IEnumerable<IKeyboardButton>> _buttons;
+	public sealed class VerticalKeyboardMarkup : IEnumerable<IEnumerable<IKeyboardButton>>
+	{
+		private readonly List<IEnumerable<IKeyboardButton>> _buttons;
 
-        public InlineKeyboardMarkup InlineKeyboardMarkup =>
-            new InlineKeyboardMarkup(
-                _buttons.Select(x => x.Select(b => b as InlineKeyboardButton)));
+		public InlineKeyboardMarkup InlineKeyboardMarkup =>
+			new InlineKeyboardMarkup(
+				_buttons.Select(x => x.Select(b => b as InlineKeyboardButton)));
 
-        public ReplyKeyboardMarkup ReplyKeyboardMarkup =>
-            new ReplyKeyboardMarkup(_buttons
-                .Select(x => x.Select(b => b as KeyboardButton)));
+		public ReplyKeyboardMarkup ReplyKeyboardMarkup =>
+			new ReplyKeyboardMarkup(_buttons
+				.Select(x => x.Select(b => b as KeyboardButton)));
 
-        public VerticalKeyboardMarkup()
-        {
-            _buttons = new List<IEnumerable<IKeyboardButton>>();
-        }
+		public VerticalKeyboardMarkup()
+		{
+			_buttons = new List<IEnumerable<IKeyboardButton>>();
+		}
 
-        public VerticalKeyboardMarkup(IEnumerable<IKeyboardButton> buttons) : this()
-        {
-            _buttons.AddRange(buttons.Select(x => new[] {x}));
-        }
+		public VerticalKeyboardMarkup(IEnumerable<IKeyboardButton> buttons) : this()
+		{
+			_buttons.AddRange(buttons.Select(x => new[] {x}));
+		}
 
-        public void Add(IKeyboardButton button)
-        {
-            if (button == null)
-                throw new ArgumentNullException(nameof(button));
+		public void Add(IKeyboardButton button)
+		{
+			if (button == null)
+				throw new ArgumentNullException(nameof(button));
 
-            _buttons.Add(new[] {button});
-        }
+			_buttons.Add(new[] {button});
+		}
 
-        public void AddRange(IEnumerable<IKeyboardButton> buttons)
-        {
-            if (buttons == null)
-                throw new ArgumentNullException(nameof(buttons));
+		public void AddRange(IEnumerable<IKeyboardButton> buttons)
+		{
+			if (buttons == null)
+				throw new ArgumentNullException(nameof(buttons));
 
-            foreach (var button in buttons)
-                Add(button);
-        }
+			foreach (var button in buttons)
+				Add(button);
+		}
 
-        public void AddRow(IEnumerable<IKeyboardButton> buttons)
-        {
-            if (buttons == null)
-                throw new ArgumentNullException(nameof(buttons));
+		public void AddRow(IEnumerable<IKeyboardButton> buttons)
+		{
+			if (buttons == null)
+				throw new ArgumentNullException(nameof(buttons));
 
-            var keyboardButtons = buttons as IKeyboardButton[] ?? buttons.ToArray();
-            if (!keyboardButtons.Any())
-                throw new ArgumentException($"Empty collection: {nameof(buttons)}");
+			var keyboardButtons = buttons as IKeyboardButton[] ?? buttons.ToArray();
+			if (!keyboardButtons.Any())
+				throw new ArgumentException($"Empty collection: {nameof(buttons)}");
 
-            _buttons.Add(keyboardButtons);
-        }
-    }
+			_buttons.Add(keyboardButtons);
+		}
+
+		public IEnumerator<IEnumerable<IKeyboardButton>> GetEnumerator()
+			=> _buttons.GetEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator()
+			=> GetEnumerator();
+	}
 }
